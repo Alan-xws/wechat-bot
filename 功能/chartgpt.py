b1a443e95aacd492e44f0780a7e577f1c8740cc8
alan_xws <1477678682@qq.com>
2024-07-19 22:41:46 +0800

    update

--- 功能/chartgpt.py
+++ 功能/chartgpt.py
@@ -3,10 +3,7 @@ import json
 
 def quary(s):
     msg = s
-    sess = requests.get(
-        ('https://open.drea.cc/bbsapi/chat/get?keyWord=' + msg + '&userName=type%3Dbbs'))
+    sess = requests.get('https://open.drea.cc/bbsapi/chat/get?keyWord=' + msg + '&userName=type%3Dbbs')
     js = sess.text
     js = json.loads(js)
-    return js['data']['reply'];
-
-
+    return js['data']['reply']
